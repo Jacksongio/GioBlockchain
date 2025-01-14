42af0165f7d387d93c57413b4bee0297ab00a580
Jack Giordano <jacksongio@GioMac.local>
2025-01-13 20:22:43 -0500

    users

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from blockchain import Blockchain
+from users import User
+
 
 # Initialize the blockchain
 my_blockchain = Blockchain()
 # sender = "Jack"
 # my_blockchain.add_balance(sender, 100)
 # Add transactions
-my_blockchain.add_transaction({"sender": "Jack", "recipient": "Troy", "amount": 10})
+Jack = User("Jack", 100)
+Jack.to_string()
+my_blockchain.add_transaction({"sender": Jack, "recipient": "Troy", "amount": 10})
 
 # Mine transactions
 print("Mining...")
